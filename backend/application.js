5d56d3afed684345aa4e42442529a2a1880dfdce
doguhannilt <doguhannilt@gmail.com>
2024-12-19 06:47:52 +0300

    Routes

--- backend/application.js
+++ backend/application.js
@@ -2,12 +2,22 @@ import express from 'express'
 import dotenv from 'dotenv'
 
 import userRoutes from './src/routes/userRoutes.js'
+import authRoutes from './src/routes/authRoutes.js'
+import adminRoutes from './src/routes/adminRoutes.js'
+import songRoutes from './src/routes/songRoutes.js'
+import albumRoutes from './src/routes/albumRoutes.js'
+import statRoutes from './src/routes/statRoutes.js'
 
 const app = express()
 dotenv.config()
 
 
 app.use("/api/users",userRoutes)
+app.use("/api/auth",authRoutes)
+app.use("/api/admin",adminRoutes)
+app.use("/api/songs",songRoutes)
+app.use("/api/albums",albumRoutes)
+app.use("/api/stats",statRoutes)
 
 
 app.listen(process.env.PORT, () => console.log('Server running'))
